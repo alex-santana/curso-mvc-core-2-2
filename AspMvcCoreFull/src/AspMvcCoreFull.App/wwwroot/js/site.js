30ccdf4c2ac9ff04e44fbbd634df07c092a5fc85
Alex <alexsan11@hotmail.com>
2019-09-17 21:24:08 -0300

    api CEP

--- AspMvcCoreFull/src/AspMvcCoreFull.App/wwwroot/js/site.js
+++ AspMvcCoreFull/src/AspMvcCoreFull.App/wwwroot/js/site.js
@@ -34,3 +34,52 @@
         });
     }
 }
+function BuscaCep() {
+    $(document).ready(function () {
+        function limpa_formulario_cep() {
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        $("#Endereco_Cep").blur(function () {
+            var cep = $(this).val().replace(/\D/g, '');
+
+            if (cep != "") {
+
+                var validaCep = /^[0-9]{8}$/;
+
+                if (validaCep.test(cep)) {
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+                            if (!("errro" in dados)) {
+
+                                $("#Endereco_Logradouro").val(dados.logradouro);
+                                $("#Endereco_Bairro").val(dados.bairro);
+                                $("#Endereco_Cidade").val(dados.localidade);
+                                $("#Endereco_Estado").val(dados.uf);
+                                console.log(dados);
+                            } else {
+                                limpa_formulario_cep();
+                                alert("Cep não encontrado");
+                            }
+                        });
+                } else {
+                    limpa_formulario_cep();
+                    alert("Formato de CEP inválido");
+                }
+
+            }else {
+
+                limpa_formulario_cep();
+                
+            }
+        });
+    });
+}
